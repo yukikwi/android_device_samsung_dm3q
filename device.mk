68b1e501799a6c36bd2502a1a0f0319260abd46e
chaptsand <chaptsand@gmail.com>
2024-09-18 21:17:26 -0300

    dm3q: overlay: Set status bar corner content padding
    Change-Id: I86608919fd107d07c4d51d716a1ad849f9f9f8b3

--- device.mk
+++ device.mk
@@ -22,7 +22,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
-    FrameworksResDm3q
+    FrameworksResDm3q \
+    SystemUIResDm3q
 
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/samsung/sm8550-common/common.mk)
